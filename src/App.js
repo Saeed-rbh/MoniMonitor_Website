bc7d7245bc1539e5a3628d7c404e3f41fe7b0fbd
Saeed Arabha <saeedarabha@outlook.com>
2024-08-02 00:15:42 -0400

    8-2-2024 12:15

--- src/App.js
+++ src/App.js
@@ -6,57 +6,36 @@ const Telegram = lazy(() => import("./Telegram/MoneyMonitor"));
 
 function App() {
   const [userData, setUserData] = useState(null);
-
   useEffect(() => {
-    // Initialize the Telegram Web App
     const initTelegramWebApp = () => {
-      const telegram = window.Telegram.WebApp;
-
-      // Extract user and session information
-      const user = telegram.initDataUnsafe.user;
-      const queryId = telegram.initDataUnsafe.query_id;
-
-      console.log("User ID:", user.id);
-      console.log("User Name:", user.first_name);
-      console.log("User Username:", user.username);
-      console.log("User Language Code:", user.language_code);
-      console.log("Session ID:", queryId);
-
-      // Update the component state with user data
-      setUserData({
-        userId: user.id,
-        userName: user.first_name,
-        userUsername: user.username,
-        userLanguage: user.language_code,
-        queryId: queryId,
-      });
+      if (window.Telegram && window.Telegram.WebApp) {
+        const telegram = window.Telegram.WebApp;
+        const user = telegram.initDataUnsafe.user;
+        const queryId = telegram.initDataUnsafe.query_id;
+        if (queryId) {
+          setUserData({
+            userId: user.id,
+            userName: user.first_name,
+            userUsername: user.username,
+            userLanguage: user.language_code,
+            queryId: queryId,
+          });
+        }
+      }
     };
-
-    // Call the function to initialize the Web App
     initTelegramWebApp();
   }, []);
 
   return (
-    // <Router>
-    <div className="App">
-      <h1>Welcome to MoniMonitor</h1>
-      {userData ? (
-        <div>
-          <p>User ID: {userData.userId}</p>
-          <p>User Name: {userData.userName}</p>
-          <p>Username: {userData.userUsername}</p>
-          <p>Language Code: {userData.userLanguage}</p>
-        </div>
-      ) : (
-        <p>Loading user data...</p>
-      )}
-      {/* //     <Suspense fallback={<div>Loading....</div>}>
-    //       <Routes>
-    //         <Route path="/" element={<Telegram />} />
-    //       </Routes>
-    //     </Suspense> */}
-    </div>
-    // </Router>
+    <Router>
+      <div className="App">
+        <Suspense fallback={<div>Loading....</div>}>
+          <Routes>
+            <Route path="/" element={<Telegram />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
   );
 }
 
