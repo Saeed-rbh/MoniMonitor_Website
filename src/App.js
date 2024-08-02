601528eae484fbb74ad755c158dc1e711fb25f77
Saeed Arabha <saeedarabha@outlook.com>
2024-08-02 00:05:49 -0400

    8-2-2024 12:05

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import "./App.css";
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const Telegram = lazy(() => import("./Telegram/MoneyMonitor"));
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    // Initialize the Telegram Web App
+    const initTelegramWebApp = () => {
+      const telegram = window.Telegram.WebApp;
+
+      // Extract user and session information
+      const user = telegram.initDataUnsafe.user;
+      const queryId = telegram.initDataUnsafe.query_id;
+
+      console.log("User ID:", user.id);
+      console.log("User Name:", user.first_name);
+      console.log("User Username:", user.username);
+      console.log("User Language Code:", user.language_code);
+      console.log("Session ID:", queryId);
+
+      // Update the component state with user data
+      setUserData({
+        userId: user.id,
+        userName: user.first_name,
+        userUsername: user.username,
+        userLanguage: user.language_code,
+        queryId: queryId,
+      });
+    };
+
+    // Call the function to initialize the Web App
+    initTelegramWebApp();
+  }, []);
+
   return (
     <Router>
       <div className="App">
