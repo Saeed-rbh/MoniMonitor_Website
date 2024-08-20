1086ca6e6152a3fc41a44408766f2c9c1b1420a1
Saeed Arabha <saeedarabha@outlook.com>
2024-08-19 20:41:28 -0400

    8-19-2024 20:41

--- src/App.js
+++ src/App.js
@@ -1,109 +1,98 @@
-// import "./App.css";
-// import React, { lazy, Suspense, useEffect, useState } from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Header from "./Header/header";
-// import { useTransactionData } from "./Tools/tools";
-
-// const Telegram = lazy(() => import("./Telegram/MoneyMonitor"));
-// const Transactions = lazy(() => import("./Transactions/Transactions"));
-// const AddTransaction = lazy(() => import("./AddTransaction/AddTransaction"));
-
-// function App() {
-//   const [userData, setUserData] = useState({
-//     userId: "",
-//     userName: "",
-//     userUsername: "",
-//     userLanguage: "",
-//     queryId: "",
-//   });
-
-//   useEffect(() => {
-//     const initTelegramWebApp = () => {
-//       if (window.Telegram && window.Telegram.WebApp) {
-//         const telegram = window.Telegram.WebApp;
-//         const user = telegram.initDataUnsafe.user;
-//         const queryId = telegram.initDataUnsafe.query_id;
-//         if (queryId) {
-//           setUserData({
-//             userId: user.id,
-//             userName: user.first_name,
-//             userUsername: user.username,
-//             userLanguage: user.language_code,
-//             queryId: queryId,
-//           });
-//         }
-//       }
-//     };
-//     initTelegramWebApp();
-//   }, []);
-
-//   const [whichMonth, setWhichMonth] = useState(0);
-//   const monthData = useTransactionData(
-//     whichMonth,
-//     userData.userId ? userData.userId : 90260003
-//   );
-
-//   const [isMoreClicked, setIsMoreClicked] = useState(null);
-//   const [isAddClicked, setIsAddClicked] = useState(null);
-
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Suspense>
-//           <Header userData={userData} />
-//           <Telegram
-//             isMoreClicked={isMoreClicked}
-//             setIsMoreClicked={setIsMoreClicked}
-//             isAddClicked={isAddClicked}
-//             setIsAddClicked={setIsAddClicked}
-//           />
-//           <Routes>
-//             <Route path="/" element={<div />} />
-
-//             <Route
-//               path="/Transactions"
-//               element={
-//                 <Transactions
-//                   isMoreClicked={isMoreClicked}
-//                   setIsMoreClicked={setIsMoreClicked}
-//                   monthData={monthData}
-//                   whichMonth={whichMonth}
-//                   setWhichMonth={setWhichMonth}
-//                 />
-//               }
-//               userId={userData.userId}
-//             />
-//             <Route
-//               path="/AddTransaction"
-//               element={
-//                 <AddTransaction
-//                   isAddClicked={isAddClicked}
-//                   setIsClicked={setIsAddClicked}
-//                   setIsAddClicked={setIsAddClicked}
-//                 />
-//               }
-//               userId={userData.userId}
-//             />
-//           </Routes>
-//         </Suspense>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
+import "./App.css";
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Header/header";
+import { useTransactionData } from "./Tools/tools";
+
+const Telegram = lazy(() => import("./Telegram/MoneyMonitor"));
+const Transactions = lazy(() => import("./Transactions/Transactions"));
+const AddTransaction = lazy(() => import("./AddTransaction/AddTransaction"));
+
 function App() {
+  const [userData, setUserData] = useState({
+    userId: "",
+    userName: "",
+    userUsername: "",
+    userLanguage: "",
+    queryId: "",
+  });
+
+  useEffect(() => {
+    const initTelegramWebApp = () => {
+      if (window.Telegram && window.Telegram.WebApp) {
+        const telegram = window.Telegram.WebApp;
+        const user = telegram.initDataUnsafe.user;
+        const queryId = telegram.initDataUnsafe.query_id;
+        if (queryId) {
+          setUserData({
+            userId: user.id,
+            userName: user.first_name,
+            userUsername: user.username,
+            userLanguage: user.language_code,
+            queryId: queryId,
+          });
+        }
+      }
+    };
+    initTelegramWebApp();
+  }, []);
+
+  const [whichMonth, setWhichMonth] = useState(0);
+  const monthData = useTransactionData(
+    whichMonth,
+    userData.userId ? userData.userId : 90260003
+  );
+
+  const [isMoreClicked, setIsMoreClicked] = useState(null);
+  const [isAddClicked, setIsAddClicked] = useState(null);
+
   return (
     <Authenticator>
       {({ signOut, user }) => (
-        <div>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </div>
+        <Router>
+          <div className="App">
+            <Suspense>
+              <Header userData={userData} />
+              <Telegram
+                isMoreClicked={isMoreClicked}
+                setIsMoreClicked={setIsMoreClicked}
+                isAddClicked={isAddClicked}
+                setIsAddClicked={setIsAddClicked}
+              />
+              <Routes>
+                <Route path="/" element={<div />} />
+
+                <Route
+                  path="/Transactions"
+                  element={
+                    <Transactions
+                      isMoreClicked={isMoreClicked}
+                      setIsMoreClicked={setIsMoreClicked}
+                      monthData={monthData}
+                      whichMonth={whichMonth}
+                      setWhichMonth={setWhichMonth}
+                    />
+                  }
+                  userId={userData.userId}
+                />
+                <Route
+                  path="/AddTransaction"
+                  element={
+                    <AddTransaction
+                      isAddClicked={isAddClicked}
+                      setIsClicked={setIsAddClicked}
+                      setIsAddClicked={setIsAddClicked}
+                    />
+                  }
+                  userId={userData.userId}
+                />
+              </Routes>
+            </Suspense>
+          </div>
+        </Router>
       )}
     </Authenticator>
   );
